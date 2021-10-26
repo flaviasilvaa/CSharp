0ead77bb53869d02fba4b26436b870e6763baaad
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-26 15:46:22 +0100

    Add files via upload
    This is a form that belongs to the math quiz app

--- Form1.cs
+++ Form1.cs
@@ -8,10 +8,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PictureViewer
+namespace MathQuiz
 {
     public partial class Form1 : Form
     {
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+
+        // These integer variables store the numbers 
+        // for the addition problem. 
+        int addend1;
+        int addend2;
+
+        // These integer variables store the numbers 
+        // for the subtraction problem. 
+        int minuend;
+        int subtrahend;
+
+
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+
+
+        // This integer variable keeps track of the 
+        // remaining time.
+        int timeLeft;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -21,55 +53,126 @@ namespace PictureViewer
         {
 
         }
+        /// <summary>
+        /// Start the quiz by filling in all of the problems
+        /// and starting the timer.
+        /// </summary>
+        public void StartTheQuiz()
+        {
+            // Fill in the addition problem.
+            // Generate two random numbers to add.
+            // Store the values in the variables 'addend1' and 'addend2'.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            // Start the timer.
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+
+        }
+        /// <summary>
+        /// Check the answer to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void showButton_Click(object sender, EventArgs e)
+        private void startButton_Click(object sender, EventArgs e)
         {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
 
-            // Show the Open File dialog. If the user clicks OK, load the
-            // picture that the user chose
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
             {
-                pictureBox1.Load(openFileDialog1.FileName);
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else
+            {
+                
+                // If the user ran out of time, stop the timer, show 
+             // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
             }
         }
 
-        private void clearButton_Click(object sender, EventArgs e)
+        private void timeLabel_Click(object sender, EventArgs e)
         {
-            //Clear the picture
-            pictureBox1.Image = null;
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            // If the user selects the Stretch check box, 
-            // change the PictureBox's
-            // SizeMode property to "Stretch". If the user clears 
-            // the check box, change it to "Normal".
-            if (checkBox1.Checked)
-                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            else
-                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
-        }
-
-        private void backgroundButton_Click(object sender, EventArgs e)
-        {
-            //Show the colour dialog.box. If the user clicks OK, change the pictureBox control
-            //background to the colour the user chose.
-            if (colorDialog1.ShowDialog() == DialogResult.OK)
-                pictureBox1.BackColor = colorDialog1.Color;
-
-
-        }
-
-        private void closeButton_Click(object sender, EventArgs e)
-        {
-            //Close the form
-            this.Close();
+            timeLabel.BackColor = Color.Red;
         }
     }
 }
